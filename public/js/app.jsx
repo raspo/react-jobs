563aa2da59b66ffa2986e0d6b6e8eebd06612d53
raspo <raspaccio@gmail.com>
2015-10-19 16:52:35 -0500

    webpack hot reloading

---
+++ public/js/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './header';
+
+ReactDOM.render(
+    <Header />,
+    document.getElementById('app')
+);
+
+console.log('webpack works');
--- server.js
+++ server.js
@@ -6,16 +6,34 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 
-// mongoose.connect('mongodb://localhost/reactjobs');
+var webpack = require('webpack');
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var webpackHotMiddleware = require('webpack-hot-middleware');
+var config = require('./webpack.config');
+var compiler = webpack(config);
 
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
+
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json());
 app.use(methodOverride());
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
+app.use(webpackDevMiddleware(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+}));
+
+app.use(webpackHotMiddleware(compiler, {
+    log: console.log,
+    path: '/__webpack_hmr',
+    heartbeat: 10 * 1000
+}));
+
+// mongoose.connect('mongodb://localhost/reactjobs');
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 app.listen(3000)
