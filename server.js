5d3bfdf838b73a49127d106a566828bc2ff42e6b
raspo <raspaccio@gmail.com>
2015-11-15 13:57:06 -0600

    move to https

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+/* eslint func-names:0 */
+
+const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const https = require('https');
+const http = require('http');
 const app = express();
 const mongoose = require('mongoose');
 const morgan = require('morgan');
@@ -8,13 +13,28 @@ const methodOverride = require('method-override');
 const compress = require('compression');
 const favicon = require('serve-favicon');
 
+try { fs.statSync(path.join(__dirname + '/ssl/server.key')); } catch (e) {
+    // Here is a good guide on how to generate ssl certificates for development
+    // http://www.akadia.com/services/ssh_test_certificate.html
+    console.log('The server requires a SSL certificate');
+    process.exit(0);
+}
+
 const apiRouter = require('./server/api/router');
+const redirectHTTP = function(req, res, next) {
+    if (req.secure) {
+        next();
+    } else {
+        res.redirect('https://' + req.headers.host + req.url);
+    }
+};
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(methodOverride());
 app.use(compress());
+app.use(redirectHTTP);
 
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(express.static(__dirname + '/public'));
@@ -27,6 +47,14 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.listen(3000);
+const serverOptions = {
+    key: fs.readFileSync(path.join(__dirname + '/ssl/server.key')),
+    cert: fs.readFileSync(path.join(__dirname + '/ssl/server.crt')),
+    requestCert: false,
+    rejectUnauthorized: false
+};
 
-console.log('App listening on port 3000');
+http.createServer(app).listen(3001); // 80 for prod
+https.createServer(serverOptions, app).listen(3000, function() { // 443 for prod
+    console.log('Secure server listening on port 3000');
+});
