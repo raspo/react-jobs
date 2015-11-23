be31179c707f15712eaa9f22fa5f65fc0cd47670
raspo <raspaccio@gmail.com>
2015-11-22 21:55:36 -0600

    better 404 page

--- app/containers/not-found.jsx
+++ app/containers/not-found.jsx
@@ -1,12 +1,58 @@
+import _ from 'lodash';
 import React from 'react';
-const { Component } = React;
+const { Component, PropTypes } = React;
+import { connect } from 'react-redux';
+import DocumentTitle from 'react-document-title';
+import { getJobs } from 'actions/jobs';
+import JobList from 'components/job-list';
+import Loading from 'components/loading';
 
-export default class NotFound extends Component {
+class NotFound extends Component {
+    static propTypes = {
+        isFetching: PropTypes.bool.isRequired,
+        jobsList: PropTypes.array.isRequired,
+        dispatch: PropTypes.func.isRequired
+    }
+
+    componentDidMount() {
+        const { dispatch } = this.props;
+        dispatch(getJobs());
+    }
+
     render() {
+        const { isFetching, jobsList } = this.props;
         return (
-            <div className="page">
-                Page Not Found
-            </div>
+            <DocumentTitle title="React Jobs - Page not found">
+                <div className="page">
+                    <header className="page-header">
+                        <h2 className="page-header-title">404 - Page not found</h2>
+                    </header>
+                    <section className="page-content">
+                        <main className="main">
+                            {isFetching ? <Loading /> : <JobList jobs={jobsList} />}
+                        </main>
+                        <aside className="sidebar">
+
+                        </aside>
+                    </section>
+                </div>
+            </DocumentTitle>
         );
     }
 }
+
+function jobsSelector(state) {
+    const { filter, jobs, jobsById, isFetchingJobs } = state;
+
+    const jobsList = _.map(jobs, (jobId) => {
+        return jobsById[jobId];
+    });
+
+    return {
+        filter,
+        isFetching: isFetchingJobs,
+        jobsList: jobsList
+    };
+}
+
+export default connect(jobsSelector)(NotFound);
